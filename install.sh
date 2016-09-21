8d4381b2815279a823c9f810c4a6eef0ced4ce5e
Grzegorz Unijewski <grzegorz@unijewski.pl>
2016-09-21 22:19:14 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR EXTRA_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+mkdir -p "$HOME/.vim"
+
+# Bunch of symlinks
+ln -sfv "$DOTFILES_DIR/git/gitconfig" "$HOME/.gitconfig"
+ln -sfv "$DOTFILES_DIR/git/gitignore_global" "$HOME/.gitignore_global"
+ln -sfv "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
+ln -sfv "$DOTFILES_DIR/vim/vimrc" "$HOME/.vimrc"
+ln -sfv "$DOTFILES_DIR/vim/colors" "$HOME/.vim/colors"
+
+# Package managers & packages
+# . "$DOTFILES_DIR/install/brew.sh"
+
+echo "Done!"
