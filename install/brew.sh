d42b9e5c1756f8286b4421eb0df5cbdb272b9f54
Grzegorz Unijewski <grzegorz@unijewski.pl>
2016-09-22 02:01:18 +0200

    Add brew installation script

---
+++ install/brew.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Install Homebrew and some of the common dependencies needed/desired for software development
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Check for Homebrew and install it if missing
+if test ! $( which brew )
+then
+  echo "Installing Homebrew..."
+  ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+fi
+
+brew tap homebrew/versions
+brew tap homebrew/services
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulas
+brew upgrade --all
+
+# Install the Homebrew packages
+apps=(
+  elasticsearch
+  ffmpeg
+  git
+  htop
+  imagemagick
+  macvim --with-lua
+  mongodb
+  phantomjs
+  terminal-notifier
+  postgresql
+  openssl
+  rbenv
+  python
+  redis
+  tree
+)
+
+brew install "${apps[@]}"
+
+# Remove outdated versions from the cellar
+brew cleanup
