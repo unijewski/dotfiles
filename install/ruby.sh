ed13787824bf2099cffce3530957e63f4e461fce
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-07-15 12:44:19 +0200

    Reload bash after installing rvm

--- install/ruby.sh
+++ install/ruby.sh
@@ -3,6 +3,8 @@ brew install gpg2
 curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -sSL https://get.rvm.io | bash -s stable
 
+source ~/.profile
+
 rvm install 2.4.1
 rvm use 2.4.1 --default
 
