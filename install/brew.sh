bfac253eb0f43c16b7872d5d19c2c2897a73d1cd
Grzegorz Unijewski <grzegorz@unijewski.pl>
2017-07-15 12:42:17 +0200

    Remove deprecated homebrew/versions

--- install/brew.sh
+++ install/brew.sh
@@ -12,7 +12,6 @@ then
   ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
 fi
 
-brew tap homebrew/versions
 brew tap homebrew/services
 
 # Make sure we’re using the latest Homebrew
