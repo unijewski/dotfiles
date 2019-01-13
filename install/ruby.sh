a52190bed3a28ef80944e97d6821571755b8c631
Grzegorz Unijewski <grzegorz@unijewski.pl>
2019-01-14 00:23:41 +0100

    Update Ruby installation

--- install/ruby.sh
+++ install/ruby.sh
@@ -1,11 +1,13 @@
 brew install gpg2
 
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -sSL https://get.rvm.io | bash -s stable
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+
+rvm install 2.5.3
 
 source ~/.profile
 
-rvm install 2.5.3
 rvm use 2.5.3 --default
 
 gem install bundler
