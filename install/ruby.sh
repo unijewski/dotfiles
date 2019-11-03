e682e09007bb6602a8eca8c47575a0dd521ce5a2
Grzegorz Unijewski <grzegorz@unijewski.pl>
2019-11-03 16:27:02 +0100

    Bump default Ruby version to 2.6.5

--- install/ruby.sh
+++ install/ruby.sh
@@ -1,13 +1,13 @@
 brew install gpg2
 
-gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
-rvm install 2.6.3
+rvm install 2.6.5
 
 source ~/.profile
 
-rvm use 2.6.3 --default
+rvm use 2.6.5 --default
 
 gem install bundler
