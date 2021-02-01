a1e89999baf03b27f7b16ab13c7dc80061d59bed
Grzegorz Unijewski <grzegorz@unijewski.pl>
2021-02-01 15:47:52 +0100

    Update zsh installation URL

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,3 +1,3 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -sfv "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 ln -sfv "$DOTFILES_DIR/zsh/themes/unijewski.zsh-theme" "$HOME/.oh-my-zsh/themes/unijewski.zsh-theme"
