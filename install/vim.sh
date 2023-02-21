41b5348983caffbc8cf7deb40cf3f45404253701
Grzegorz Unijewski <grzegorz@unijewski.pl>
2023-02-21 16:55:55 +0100

    Migrate from vundle to vim-plug

--- install/vim.sh
+++ install/vim.sh
@@ -1,5 +1,5 @@
 mkdir -p "$HOME/.vim"
 ln -sfv "$DOTFILES_DIR/vim/vimrc" "$HOME/.vimrc"
 ln -sfv "$DOTFILES_DIR/vim/colors" "$HOME/.vim/colors"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
