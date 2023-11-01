c680206f0ea85a25ac682edb133e88e8c15c034e
Grzegorz Unijewski <grzegorz@unijewski.pl>
2023-11-01 23:28:02 +0100

    Force vscode extensions installation

--- install/vscode.sh
+++ install/vscode.sh
@@ -1,2 +1,2 @@
 ln -sfv "$DOTFILES_DIR/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-cat "$DOTFILES_DIR/vscode/extensions.txt" | xargs -n 1 code --install-extension
+cat "$DOTFILES_DIR/vscode/extensions.txt" | xargs -n 1 code --force --install-extension
