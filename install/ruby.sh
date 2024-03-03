4b5737d8237950f7d40cfc5c3751eca934e3d2ea
Grzegorz Unijewski <grzegorz@unijewski.pl>
2024-03-04 00:17:09 +0100

    Update Ruby installation

--- install/ruby.sh
+++ install/ruby.sh
@@ -2,12 +2,12 @@ brew install gpg2
 
 \curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+\curl -sSL https://get.rvm.io | bash
 
 source ~/.rvm/scripts/rvm
 
-rvm install 3.1.2
+rvm install 3.1.3 --with-openssl-dir=$(brew --prefix openssl)
 
-rvm use 3.1.2 --default
+rvm use 3.1.3 --default
 
 gem install bundler
